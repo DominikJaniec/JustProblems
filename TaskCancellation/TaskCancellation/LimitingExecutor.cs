f82c223d74c12cd0a21a50e4ff0491031b79c8a6
Dominik Janiec <dominikjaniec@gmail.com>
2018-10-03 23:47:50 +0200

    TaskCancelation: Unstable self-canceling

---
+++ TaskCancellation/TaskCancellation/LimitingExecutor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCancellation
+{
+    public class LimitingExecutor : IDisposable
+    {
+        private const int UpperLimit = 1;
+        private readonly Semaphore _semaphore
+            = new Semaphore(UpperLimit, UpperLimit);
+
+        public Task Do(Action work, CancellationToken token)
+            => Task.Run(() =>
+            {
+                _semaphore.WaitOne();
+                try
+                {
+                    token.ThrowIfCancellationRequested();
+                    work();
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
+            }, token);
+
+        public void Dispose()
+            => _semaphore.Dispose();
+    }
+}
---
+++ TaskCancellation/TaskCancellation/TaskCancellationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TaskCancellation
+{
+    public class TaskCancellationTests
+    {
+        [Test]
+        public void RequestPropagationTest()
+        {
+            using (var setupEvent = new ManualResetEvent(initialState: false))
+            using (var cancellation = new CancellationTokenSource())
+            using (var executor = new LimitingExecutor())
+            {
+                // System-state setup action:
+                var cancellingTask = executor.Do(() =>
+                {
+                    setupEvent.WaitOne();
+                    cancellation.Cancel();
+                }, CancellationToken.None);
+
+                // Main work action:
+                var actionTask = executor.Do(() =>
+                {
+                    throw new InvalidOperationException(
+                        "This action should be cancelled!");
+                }, cancellation.Token);
+
+                // Let's wait until this `Task` starts, so it will got opportunity
+                // to cancel itself, and expected later exception will not come
+                // from just starting that action by `Task.Run` with token:
+                while (actionTask.Status < TaskStatus.Running)
+                    Thread.Sleep(millisecondsTimeout: 1);
+
+                // Let's unblock slot in Executor for the 'main work action'
+                // by finalizing the 'system-state setup action' which will
+                // finally request "global" cancellation:
+                setupEvent.Set();
+
+                Assert.DoesNotThrowAsync(
+                    async () => await cancellingTask);
+
+                Assert.ThrowsAsync<TaskCanceledException>(
+                    async () => await actionTask);
+            }
+        }
+    }
+}
