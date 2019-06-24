9d170dcb8ebcd488922c18d5e492aad54f695142
Dominik Janiec <dominikjaniec@gmail.com>
2019-06-24 23:29:13 +0200

    NUnitInconsistency: example project to show difference between 'execute' and 'explore'

---
+++ NUnitInconsistency/NUnitInconsistency/ExecuteVsExploreTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitInconsistency
+{
+    public class ExecuteVsExploreTests
+    {
+        private const bool ShouldCauseProblems = false;
+
+        public static string[] ProblematicSource()
+        {
+            if (ShouldCauseProblems)
+                throw new Exception("Problem!");
+
+            return new[] { "First", "Omega" };
+        }
+
+        [TestCaseSource(nameof(ProblematicSource))]
+        public void InconsistencyTest(string value)
+            => Assert.IsNotEmpty(value, "Just for show");
+    }
+}
