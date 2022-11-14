82b1843d0294e33cf6ab08ce51f82a370f85d1f0
Dominik Janiec <dominikjaniec@gmail.com>
2022-11-14 16:41:38 +0100

    NUnit Test Adapter in VS 2019

---
+++ ThoseTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace SomeTests
+{
+    public class ThoseTests
+    {
+        [Test]
+        public void TheTest()
+            => Assert.That(true,
+                Is.Not.False);
+    }
+}
